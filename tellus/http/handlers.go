f5d0065d3b5a11da167c1c7d0bb01f3d6dd2084d
David Tennander <david.tennander@gmail.com>
2019-03-24 11:52:18 +0100

    Makes webhook start goroutine for each event.

--- tellus/http/handlers.go
+++ tellus/http/handlers.go
@@ -39,12 +39,17 @@ func healthzHandler() http.HandlerFunc {
 func webhookHandler(tellus *tellus.Client) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		eventType := r.Header.Get("X-GitHub-Event")
-		defer r.Body.Close()
 		switch eventType {
 		case "pull_request":
-			handlePullRequest(tellus, r.Body)
+			go func() {
+				defer r.Body.Close()
+				handlePullRequest(tellus, r.Body)
+			}()
 		case "push":
-			handlePush(tellus, r.Body)
+			go func() {
+				defer r.Body.Close()
+				handlePush(tellus, r.Body)
+			}()
 		}
 	}
 }
@@ -61,6 +66,7 @@ func handlePullRequest(tellusClient *tellus.Client, reader io.Reader) {
 		print(err.Error())
 		return
 	}
+	log.Printf("done handling pull_request event")
 }
 
 func handlePush(tellusClient *tellus.Client, reader io.Reader) {
@@ -75,5 +81,6 @@ func handlePush(tellusClient *tellus.Client, reader io.Reader) {
 		print(err.Error())
 		return
 	}
+	log.Printf("done handling push event")
 }
 
